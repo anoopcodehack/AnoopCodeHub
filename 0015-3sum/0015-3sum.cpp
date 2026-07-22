03db200affc4a562569814f4f547af5c3643f617
Anoop -A <anooparjun05@gmail.com>
2026-07-22 10:56:43 +0530

    Time: 48 ms (53.98%), Space: 29.2 MB (47.43%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,45 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
 
         vector<vector<int>> ans;
 
-        sort(nums.begin(), nums.end());
-
-        int n = nums.size();
-
-        for(int i = 0; i < n - 2; i++) {
+        for (int i = 0; i < nums.size() - 2; i++) {
 
            
-            if(i > 0 && nums[i] == nums[i - 1])
+            if (i > 0 && nums[i] == nums[i - 1])
                 continue;
 
-            int left = i + 1;
-            int right = n - 1;
+            int j = i + 1;
+            int k = nums.size() - 1;
 
-            while(left < right) {
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
 
-                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    ans.push_back({nums[i], nums[j], nums[k]});
 
-                if(sum == 0) {
+                    j++;
+                    k--;
 
-                    ans.push_back({nums[i], nums[left], nums[right]});
+                    
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
 
-                  
-                    while(left < right && nums[left] == nums[left + 1])
-                        left++;
-
-                    while(left < right && nums[right] == nums[right - 1])
-                        right--;
-
-                    left++;
-                    right--;
+                   
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
                 }
-                else if(sum < 0) {
-                    left++;
+                else if (sum < 0) {
+                    j++;
                 }
                 else {
-                    right--;
+                    k--;
                 }
             }
         }
