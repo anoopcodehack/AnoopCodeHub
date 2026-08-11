3ee0e4b8d077b0c2c1f65d5e6849acd22696e4b8
Anoop -A <anooparjun05@gmail.com>
2026-08-11 23:09:39 +0530

    Time: 83 ms (58.15%), Space: 56.2 MB (39.77%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -6,11 +6,8 @@ public:
 
 private:
     int atMost(vector<int>& nums, int k) {
-        if (k == 0) return 0;
-
         unordered_map<int, int> freq;
-        int left = 0;
-        int count = 0;
+        int left = 0, ans = 0;
 
         for (int right = 0; right < nums.size(); right++) {
             freq[nums[right]]++;
@@ -24,9 +21,9 @@ private:
                 left++;
             }
 
-            count += right - left + 1;
+            ans += right - left + 1;
         }
 
-        return count;
+        return ans;
     }
 };
