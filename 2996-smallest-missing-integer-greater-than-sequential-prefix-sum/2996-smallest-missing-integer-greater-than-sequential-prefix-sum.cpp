d5cbca67b18750a9da66e75c7b3bd9a04a70d142
Anoop -A <anooparjun05@gmail.com>
2026-08-11 20:40:09 +0530

    Time: 0 ms (100%), Space: 23.1 MB (29.32%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -4,16 +4,16 @@ public:
         int sum = nums[0];
 
         for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] == nums[i - 1] + 1) {
+            if (nums[i] == nums[i - 1] + 1)
                 sum += nums[i];
-            } else {
+            else
                 break;
-            }
         }
 
-        while (find(nums.begin(), nums.end(), sum) != nums.end()) {
+        unordered_set<int> st(nums.begin(), nums.end());
+
+        while (st.count(sum))
             sum++;
-        }
 
         return sum;
     }
