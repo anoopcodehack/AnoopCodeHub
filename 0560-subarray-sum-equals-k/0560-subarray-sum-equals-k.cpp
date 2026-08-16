06e92f1055433fd56871fa5ffa0f9a4d1755599a
Anoop -A <anooparjun05@gmail.com>
2026-08-16 23:02:59 +0530

    Time: 2463 ms (16.88%), Space: 34.4 MB (80.8%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
-mp[0] = 1;
-
-int prefix = 0;
-int ans = 0;
-
-for (int x : nums) {
-    prefix += x;
-
-    if (mp.count(prefix - k))
-        ans += mp[prefix - k];
-
-    mp[prefix]++;
-}
-
-return ans;
-        
+       int c=0;
+       int n=nums.size();
+       for(int l=0;l<n;l++){
+        int sum=0;
+        for(int r=l;r<n;r++){
+            sum+=nums[r];
+            if(sum==k){
+                c++;
+            }
+        }
+       }
+       return c;
     }
 };
