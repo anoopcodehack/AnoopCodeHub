f644a40329357971169012446fa6396b39bc2e8d
Anoop -A <anooparjun05@gmail.com>
2026-09-21 11:34:32 +0530

    Time: 7 ms (90.95%), Space: 35 MB (87.71%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
-    int shipWithinDays(vector<int>& weights, int days) {
-        int low = *max_element(weights.begin(), weights.end());
-        int high = accumulate(weights.begin(), weights.end(), 0);
+    bool canShip(vector<int>& weights, int days, int capacity) {
+        int day = 1;
+        int currentWeight = 0;
 
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-
-            int d = 1, sum = 0;
-
-            for (int w : weights) {
-                if (sum + w > mid) {
-                    d++;
-                    sum = 0;
-                }
-                sum += w;
+        for (int weight : weights) {
+            if (currentWeight + weight <= capacity) {
+                currentWeight += weight;
+            } else {
+                day++;
+                currentWeight = weight;
             }
-
-            if (d <= days)
-                high = mid;
-            else
-                low = mid + 1;
         }
 
-        return low;
+        return day <= days;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int left = *max_element(weights.begin(), weights.end());
+        int right = accumulate(weights.begin(), weights.end(), 0);
+
+        int ans = right;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (canShip(weights, days, mid)) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return ans;
     }
 };
